3c97cb1d61c8667e2b757fe469f1ed060ee79281
lalithrajr1034 <placement1034@gmail.com>
2025-06-17 15:09:38 +0530

    updated

---
+++ main.py
@@ -0,0 +1,19 @@
+#shows
+print("""
+      1.morning show 
+      2.afternoon show
+      3.Night show""")
+movieShows=int(input("Enter the shows: "))
+if movieShows==1:
+    print("You have selected Morning show")
+    pass
+elif movieShows==2:
+    print("You have selected After noon show")
+elif movieShows==3:
+    print("You have selected Night show")
+    pass    
+    
+
+
+#movie's
+#seat type
